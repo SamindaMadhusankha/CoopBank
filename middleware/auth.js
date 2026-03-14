06352e2038dc6d18271fcc20c5ea1a5ff9ebf503
Saminda Madhusankha <samindamadhusankha1@gmail.com>
2026-03-14 18:35:57 +0530

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+// Authentication middleware
+
+function requireLogin(req, res, next) {
+  if (!req.session.user) {
+    return res.status(401).json({ error: 'Please log in to access this resource.' });
+  }
+  next();
+}
+
+function requireAdmin(req, res, next) {
+  if (!req.session.user) {
+    return res.status(401).json({ error: 'Please log in to access this resource.' });
+  }
+  if (req.session.user.role !== 'admin') {
+    return res.status(403).json({ error: 'Administrator access required.' });
+  }
+  next();
+}
+
+module.exports = { requireLogin, requireAdmin };
